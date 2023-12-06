18f076c024b9d55c7dfe3b65c16800fce0808013
Kimbada1 <153051258+Kimbada1@users.noreply.github.com>
2023-12-06 18:36:41 +0900

    Gradle Scipts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.clp3"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.clp3"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    buildFeatures{
-
-        viewBinding = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-
-    implementation("com.naver.maps:map-sdk:3.17.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("com.google.android.gms:play-services-location:latest_version")
-    implementation ("com.android.support:multidex:1.0.3")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
-    implementation("com.google.android.material:material:1.12.0-alpha01")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    id("com.android.application") version "8.1.2" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
