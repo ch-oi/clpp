bb8882d465d65c517f2238454dbfddf9937e2cd2
Kimbada1 <153051258+Kimbada1@users.noreply.github.com>
2023-12-13 14:24:44 +0900

    Update AddFragment.java

--- clp3/AddFragment.java
+++ clp3/AddFragment.java
@@ -4,6 +4,8 @@ import static android.app.Activity.RESULT_OK;
 
 import static com.google.firebase.appcheck.internal.util.Logger.TAG;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.WindowDecorActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -47,7 +50,12 @@ public class AddFragment extends Fragment {
 
     ImageView iv;
     private Button cl, cs, cu;
+    TextView ttv;
     Uri imgUri;
+
+    private String[] items = {"문수지", "2호관", "벽강아트센터"};
+    private boolean[] checkedItems = {false, false, false};
+    private StringBuilder selectedItems = new StringBuilder();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -60,25 +68,32 @@ public class AddFragment extends Fragment {
         cu = view.findViewById(R.id.cu);
         cs = view.findViewById(R.id.cs);
         cl = view.findViewById(R.id.cl);
+        ttv = view.findViewById(R.id.ttv);
 
 
+        //cl.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View v) {
+
+               // FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+               // StorageReference rootRef = firebaseStorage.getReference("uploads/");
+               // StorageReference imgRef = rootRef.child("20231207010556.png");
+
+               // if (imgRef != null) {
+                   // imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                       // @Override
+                       // public void onSuccess(Uri uri) {
+                            //Glide.with(AddFragment.this).load(uri).into(iv);
+                       // }
+                   // });
+
+               // }
+            //}
+        //});
         cl.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-                StorageReference rootRef = firebaseStorage.getReference();
-                StorageReference imgRef = rootRef.child("자신감_지킴이_화이트_pc.png");
-
-                if (imgRef != null) {
-                    imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            Glide.with(AddFragment.this).load(uri).into(iv);
-                        }
-                    });
-
-                }
+            public void onClick(View view) {
+                showInputDialog();
             }
         });
 
@@ -119,11 +134,53 @@ public class AddFragment extends Fragment {
                 } else {
                     // imgUri가 null인 경우 처리, 예를 들어 오류 메시지를 표시할 수 있습니다.
                     Log.e(TAG, "Image URI is null");
+                    Toast.makeText(getContext(), "이미지가 없음", Toast.LENGTH_SHORT).show();
                 }
             }
         });
         return view;
     }
+
+    @SuppressLint("RestrictedApi")
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Select Items");
+        builder.setMultiChoiceItems(items, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                checkedItems[which] = isChecked;
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedItems.setLength(0); // Clear previous selections
+                for (int i = 0; i < checkedItems.length; i++) {
+                    if (checkedItems[i]) {
+                        selectedItems.append(items[i]).append(", ");
+                    }
+                }
+
+                // Remove trailing comma and space
+                if (selectedItems.length() > 0) {
+                    selectedItems.delete(selectedItems.length() - 2, selectedItems.length());
+                }
+
+                ttv.setText(selectedItems.toString());
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing, just close the dialog
+            }
+        });
+
+        builder.show();
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
         super.onActivityResult(requestCode, resultCode, data);
